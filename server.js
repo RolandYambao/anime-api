69e7fdc6788c8cf98652f8bff2de792c890990bf
RolandYambao <rolandyambao@gmail.com>
2021-12-23 15:19:28 -0800

    Updated

--- server.js
+++ server.js
@@ -2,15 +2,6 @@ const express = require("express");
 const { Season } = require('./models');
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
-
 app.use(express.json());
 app.get("/seasons", async (request, response) => {
     try {
