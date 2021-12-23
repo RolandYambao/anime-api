cb98b9a90cb544c6f85940930bc036e1e4c5adbd
RolandYambao <rolandyambao@gmail.com>
2021-12-23 11:45:21 -0800

    Added Season

---
+++ models/season.js
@@ -0,0 +1,14 @@
+// our first collection schema
+// season schema
+const mongoose = require("mongoose");
+
+const seasonSchema = new mongoose.SchemaType({
+    url: String,
+    title: { type: String, required: true },
+    imague_url: String,
+    synopsis: String,
+});
+
+const Season = mongoose.model("Season", seasonSchema);
+
+module.exports = Season;
